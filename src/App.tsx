f16b25d2768b3e152c0fd1ce7e69b24bdfee4e98
qvantt <2275399@gmail.com>
2022-07-10 15:47:36 +0300

    show boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
+import { Col, Layout, Row } from 'antd';
+import { Content, Footer, Header } from 'antd/lib/layout/layout';
+import { useState } from 'react';
+import "antd/dist/antd.css";
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App: React.FC = () => {
+
+  const cols = [];
+  const colCount = 4;
+
+  for (let i = 0; i < colCount; i++) {
+    cols.push(
+      <Col key={i.toString()} span={24 / colCount}>
+        <div>Column</div>
+      </Col>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Row>
+        <Header className='accounts-area'>Header</Header>
+      </Row>
+      <Content className='content'>
+        <Row gutter={[16, 16]}>
+          {cols}
+          {cols}
+        </Row>
+        Another Row:
+        <Row gutter={[16, 16]}>{cols}</Row>
+      </Content>
+      <Row>
+        <Col>
+          <Footer className='footer'>Footer</Footer>
+        </Col>
+      </Row>
+    </Layout>
   );
 }
 
