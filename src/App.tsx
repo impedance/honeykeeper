8cd25fc85712912e51eba8bd2f6006365b9cba05
qvantt <2275399@gmail.com>
2022-07-17 19:50:21 +0300

    add amount

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,68 @@
-import { Col, Layout, Row } from 'antd';
-import { Content, Footer, Header } from 'antd/lib/layout/layout';
+import { Col, Layout, Row } from "antd";
+import { Content, Footer, Header } from "antd/lib/layout/layout";
 import "antd/dist/antd.css";
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
+//interface IAccount {
+// name: string,
+//amount: number
+//}
+
+const BudgetItem = () => {
+  const [name, setName] = useState<string>("Default name");
+  const [amount, setAmount] = useState<number>(10);
+  const [isChanging, setIsChanging] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsChanging(!isChanging);
+  };
+
+  const handleChange = (event: { target: { value: any } }) => {
+    const { value } = event.target;
+    setAmount(value);
+  };
+  return (
+    <Col span={6}>
+      <div className="budget-item">
+        <p>{name}</p>
+        <p>{amount}</p>
+        <button onClick={handleClick}>
+          {isChanging ? "enough" : "change amount"}
+        </button>
+        {isChanging && <input onChange={handleChange} value={amount}></input>}
+      </div>
+    </Col>
+  );
+};
 
 const App: React.FC = () => {
-
   const [items, setItems] = useState<JSX.Element[]>([]);
+  const [totalAmount, setTotalAmount] = useState<number>(0);
 
   const addItem = () => {
-    const newItem =
-      <Col span={6}>
-        <div>Item</div>
-      </Col>
-    setItems([...items, newItem])
-  }
+    setItems([...items, <BudgetItem key={items.length + 1} />]);
+  };
 
   return (
     <Layout>
       <Row>
         <Col>
-          <Header className='accounts-area' onClick={addItem}>Header</Header>
+          <Header onClick={addItem}>
+            <div className="accounts-area">Header</div>
+          </Header>
         </Col>
       </Row>
-      <Content className='content'>
-        <Row gutter={[32, 32]}>
-          {items}
-        </Row>
+      <Content className="content">
+        <Row gutter={[32, 32]}>{items}</Row>
       </Content>
       <Row>
         <Col>
-          <Footer className='footer'>Footer</Footer>
+          <Footer className="footer">Footer</Footer>
         </Col>
       </Row>
     </Layout>
   );
-}
+};
 
 export default App;
