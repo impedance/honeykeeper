d6c8efe643657eb2ef434ecc8dd6f89347974d7e
qvantt <2275399@gmail.com>
2022-07-25 21:26:09 +0300

    add deselect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Col, Layout, Row } from "antd";
+import { Form, Input, Button, Col, Layout, Row } from "antd";
 import { Content, Footer, Header } from "antd/lib/layout/layout";
 import "antd/dist/antd.css";
 import React, { FC, useState } from "react";
@@ -16,12 +16,37 @@ const App: FC = () => {
   const [budgets, setBudgets] = useState<{ [key: string]: BudgetItem }>({});
   const [counter, setCounter] = useState<number>(0);
   const [selectedId, setSelectedId] = useState<string>("");
+  const [sumToGet, setSumToGet] = useState<number>(0);
+  const [sumToReturn, setSumToReturn] = useState<number>(0);
+
+  enum Operation {
+    GET,
+    RETURN,
+  }
+  const handleChange = (
+    event: { target: { value: any } },
+    operation: Operation
+  ) => {
+    const { value } = event.target;
+    const sum = Number(value);
+    if (operation === Operation.GET) {
+      setSumToGet(sum);
+    } else {
+      setSumToReturn(sum);
+    }
+  };
 
   const changeBudgetAmount = (id: string, sum: number) => {
     const { amount } = budgets[id];
     const newBudget = { ...budgets[id], amount: amount + sum };
     setTotalAmount(totalAmount - sum);
     setBudgets({ ...budgets, [id]: newBudget });
+  };
+  const getSum = () => {
+    changeBudgetAmount(selectedId, sumToGet);
+  };
+  const returnSum = () => {
+    changeBudgetAmount(selectedId, -sumToReturn);
   };
 
   const addBudget = () => {
@@ -50,7 +75,7 @@ const App: FC = () => {
     } else {
       setBudgets({ ...budgets, [budgetId]: newSelectedBudget });
     }
-    setSelectedId(budgetId);
+    setSelectedId(budgetId === selectedId ? "" : budgetId);
   };
 
   const budgetItems = Object.entries(budgets).map(([key, value]) => {
@@ -61,7 +86,6 @@ const App: FC = () => {
         key={key}
         budgetId={key}
         title={title}
-        changeAmount={changeBudgetAmount}
         selected={selected}
         deselect={deselect}
       />
@@ -72,12 +96,43 @@ const App: FC = () => {
     <Layout>
       <Row>
         <Col span={24}>
-          <Header onClick={addBudget}>
-            <div className="accounts-area">
-              <div className="total-amount">{totalAmount}</div>
-              <div>selected: {selectedId}</div>
-            </div>
-          </Header>
+          <div className="accounts-area">
+            <div className="total-amount">{totalAmount}</div>
+            <Button onClick={addBudget}>add new card</Button>
+            <div>selected: {selectedId}</div>
+            {selectedId && (
+              <Form>
+                <Form.Item>
+                  <Row>
+                    <Col>
+                      <Button onClick={getSum}>Get sum</Button>
+                    </Col>
+                    <Col span={6}>
+                      <Input
+                        value={sumToGet}
+                        onChange={(event) => handleChange(event, Operation.GET)}
+                      />
+                    </Col>
+                  </Row>
+                </Form.Item>
+                <Form.Item>
+                  <Row>
+                    <Col>
+                      <Button onClick={returnSum}>Return sum</Button>
+                    </Col>
+                    <Col span={6}>
+                      <Input
+                        value={sumToReturn}
+                        onChange={(event) =>
+                          handleChange(event, Operation.RETURN)
+                        }
+                      />
+                    </Col>
+                  </Row>
+                </Form.Item>
+              </Form>
+            )}
+          </div>
         </Col>
       </Row>
       <Content className="content">
--- src/BudgetCard.tsx
+++ src/BudgetCard.tsx
@@ -6,80 +6,23 @@ interface Props {
   amount: number;
   title?: string;
   selected: boolean;
-  changeAmount: (id: string, budgetedAmount: number) => void;
   deselect: (id: string) => void;
-}
-enum Operation {
-  GET,
-  RETURN,
 }
 
 export const BudgetCard: FC<Props> = ({
   amount,
   title,
   budgetId,
-  changeAmount,
   selected,
   deselect
 }) => {
-  const [isChanging, setIsChanging] = useState<boolean>(false);
   const [currentAmount, setCurrentAmount] = useState<number>(amount || 0);
-  const [sumToGet, setSumToGet] = useState<number>(0);
-  const [sumToReturn, setSumToReturn] = useState<number>(0);
-
-  const handleChange = (
-    event: { target: { value: any } },
-    operation: Operation
-  ) => {
-    const { value } = event.target;
-    const sum = Number(value);
-    if (operation === Operation.GET) {
-      setSumToGet(sum);
-    } else {
-      setSumToReturn(sum);
-    }
-  };
-
-  const getSum = () => {
-    changeAmount(budgetId, sumToGet);
-  };
-  const returnSum = () => {
-    changeAmount(budgetId, -sumToReturn);
-  };
   return (
     <Col span={8} onClick={() => deselect(budgetId)}>
       <div className={`budget-item ${selected ? "selected-budget" : ""}`}>
         <p>
           {title}----{amount}
         </p>
-        <Form>
-          <Form.Item>
-            <Row>
-              <Col>
-                <Button onClick={getSum}>Get sum</Button>
-              </Col>
-              <Col span={6}>
-                <Input
-                  value={sumToGet}
-                  onChange={(event) => handleChange(event, Operation.GET)}
-                />
-              </Col>
-            </Row>
-          </Form.Item>
-          <Form.Item>
-            <Row>
-              <Col>
-                <Button onClick={returnSum}>Return sum</Button>
-              </Col>
-              <Col span={6}>
-                <Input
-                  value={sumToReturn}
-                  onChange={(event) => handleChange(event, Operation.RETURN)}
-                />
-              </Col>
-            </Row>
-          </Form.Item>
-        </Form>
       </div>
     </Col>
   );
